a13bb2073deb4b1f45cdaeaf100ade0e229c2934
curtis <crutchc@yahoo.com>
2019-10-29 14:20:32 -0500

    files set up and work like they are supposed to

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,9 @@
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string BeanType { get; set; }
+    }
+}
---
+++ CoffeeShopTabeCreationSQL.sql
@@ -0,0 +1,16 @@
+﻿DROP TABLE IF EXISTS Coffee;
+
+CREATE TABLE Coffee (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    Title VARCHAR(50) NOT NULL,
+    BeanType VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Coffee (Title, BeanType)
+VALUES ('Espresso', 'Brazilian');
+
+INSERT INTO Coffee (Title, BeanType)
+VALUES ('Cafe Con Leche', 'Costa Rican');
+
+INSERT INTO Coffee (Title, BeanType)
+VALUES ('Cappuccino', 'Guatemalan');
